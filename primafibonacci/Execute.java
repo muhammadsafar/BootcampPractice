b55ce6acf6f055172a98b1a317a8260727ad004f
Muhammad Safar Abdurrahman <muhammad.ict1487@gmail.com>
2018-07-25 11:41:44 +0800

    Execute.java
    update documentation, delete variable fibarray[i];

--- primafibonacci/Execute.java
+++ primafibonacci/Execute.java
@@ -42,64 +42,32 @@ public class Execute {
     }
 
     /**
-     *
-     * @param firstValues nilai pertama
-     * @param secondValues nilai kedua
-     * @param batas batas akhir perhitungan fibonacci
-     * @return
+     * 
+     * @param firstValues nilai pertama dari bilangan fibonacci
+     * @param secondValues nilai kedua dari bilangan fibonacci
+     * @param batas batas pengecekan bilangan fibonacci
+     * @return 
      */
     public String Fibo(int firstValues, int secondValues, int batas) {
         String hasilString = "";
 
-        long fibarray[] = new long[batas];
+        int fibarray[] = new int[batas];
 
-        fibarray[0] = firstValues;
-        fibarray[1] = secondValues;
-
-        for (int i = 2; i < batas; i++) {
-            fibarray[i] = fibarray[i - 1] + fibarray[i - 2];
-
+        for (int i = 0; i < batas; i++) {
+            
+            if (i == firstValues || i == secondValues) {
+                fibarray[i] = i;
+            } else {
+                fibarray[i] = fibarray[i - 1] + fibarray[i - 2];
+            }
         }
+       
         for (int i = 0; i < batas; i++) {
             if (fibarray[i] <= batas) {
                 System.out.print(fibarray[i] + " ");
-            } else {
-                break;
             }
         }
         return hasilString;
-    }
-
-    public String binner(int bil) {
-        String lihat = "";
-
-        int binner[] = {00000000, 00000001, 00000010, 00000100, 00001000, 00010000, 00100000, 01000000, 10000000};
-        char hexa[] = {'A', 'B', 'C', 'D', 'E', 'F'};
-
-        int a;
-        int batas;
-
-        for (int i = 0; i < 100; i++) {
-
-        }
-        return lihat;
-    }
-
-    public int penjumlahan(int bil1, int bil2) {
-
-        int hasil = bil1 + bil2;
-        return hasil;
-
-    }
-    
-    public long faktorial(int angka) {
-
-        long jml=1;
-        for (int i = 0; i < angka; i++) {
-            jml = i*jml;
-            
-        }
-        return jml;
     }
 
 }
